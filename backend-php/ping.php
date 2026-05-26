92cf1ff0d0c1f041bbbb0d5b5063342d659143aa
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-05-26 11:09:07 -0500

    subiendo ultimos cambios, antes de eliminar, y dejar todo en el remoto

--- backend-php/ping.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Script de prueba sin dependencias
-echo "pong - server is alive and .htaccess is working";
--- backend-php/read_log.php
+++
@@ -1,9 +0,0 @@
-<?php
-$logFile = 'logs/app.log';
-if (file_exists($logFile)) {
-    $lines = file($logFile);
-    $lastLines = array_slice($lines, -50);
-    echo implode("", $lastLines);
-} else {
-    echo "Log file not found.";
-}
